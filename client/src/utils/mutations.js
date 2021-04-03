1304442dcca18f75853e7eaa4fb291f85fa86baf
aldrinburgos <burgos.aldrin@gmail.com>
2021-04-03 17:30:58 -0500

    finalizing

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation deleteBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      savedBooks {
+        bookId
+      }
+    }
+  }
+`;
